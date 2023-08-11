beb379148e950b65ae38a9df6554ffc64e1ca174
dspataru <61765352+dspataru@users.noreply.github.com>
2023-08-10 22:20:25 -0400

    Adding final assignment files

--- EmployeeSQL/creating_tables.sql
+++ EmployeeSQL/creating_tables.sql
@@ -38,7 +38,8 @@ FROM departments;
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
 );
 
 SELECT *
@@ -49,7 +50,8 @@ CREATE TABLE dept_manager (
 	dept_no VARCHAR(5) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	CONSTRAINT dept_manager_id PRIMARY KEY (dept_no, emp_no)
 );
 
 SELECT *
@@ -60,8 +62,10 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	CONSTRAINT dept_emp_id PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT *
 FROM dept_emp;
+
